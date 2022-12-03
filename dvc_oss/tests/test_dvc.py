cc0e9cb36e222cfc3fafdcfed80a718e14602306
Daniele Trifirò <daniele@iterative.ai>
2022-12-03 17:37:16 +0100

    update template

--- dvc_oss/tests/test_dvc.py
+++ dvc_oss/tests/test_dvc.py
@@ -5,8 +5,44 @@ from dvc.testing.api_tests import (  # noqa, pylint: disable=unused-import
 from dvc.testing.remote_tests import (  # noqa, pylint: disable=unused-import
     TestRemote,
 )
+from dvc.testing.workspace_tests import TestAdd as _TestAdd
+from dvc.testing.workspace_tests import (  # noqa, pylint: disable=unused-import
+    TestGetUrl,
+)
+from dvc.testing.workspace_tests import TestImport as _TestImport
+from dvc.testing.workspace_tests import (  # noqa, pylint: disable=unused-import
+    TestLsUrl,
+)
 
 
 @pytest.fixture
 def remote(make_remote):
     yield make_remote(name="upstream", typ="oss")
+
+
+class TestImport(_TestImport):
+    @pytest.fixture
+    def stage_md5(self):
+        raise NotImplementedError
+
+    @pytest.fixture
+    def is_object_storage(self):
+        raise NotImplementedError
+
+    @pytest.fixture
+    def dir_md5(self):
+        raise NotImplementedError
+
+
+class TestAdd(_TestAdd):
+    @pytest.fixture
+    def hash_name(self):
+        raise NotImplementedError
+
+    @pytest.fixture
+    def hash_value(self):
+        raise NotImplementedError
+
+    @pytest.fixture
+    def dir_hash_value(self):
+        raise NotImplementedError
